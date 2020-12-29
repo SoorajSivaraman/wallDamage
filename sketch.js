291be62a26073dd37aff33d90e3d21424a6e9569
SoorajSivaraman <75786247+SoorajSivaraman@users.noreply.github.com>
2020-12-29 22:23:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var startTime = null;
 var latestTime = null;
 var elapsedTime = 0;
 var gameState = "";
-var bulletCreated = false;
+var thicknessChanged = false;
 
 function setup() 
 {
@@ -14,8 +14,9 @@ function setup()
   weight = 0;
   thickness = 0;
   damage = 0;
-  bullet = null;
-  wall = null;
+  createBullet();
+  wall = createSprite(1500, 196, thickness, 322);
+  wall.shapeColor = "white";
 }
 
 function draw()
@@ -32,24 +33,27 @@ function draw()
   {
     speed = 0;
     weight = 0;
-    thickness = 0;
     damage = 0;
+    bullet.x = 50;
+    bullet.y = 200;
+    if(thicknessChanged === false) 
+    {
+      thickness = random(22, 83);
+      thicknessChanged = true;
+    }  
+    wall.width = thickness;
+    wall.shapeColor = "white";
     gameState = "play";
-    bulletCreated = false;
   }
   if(gameState === "play")
   {
-    thickness = random(22, 83);
     speed = random(223, 321);
     weight = random(30, 52);
-    if(wall != null) wall.destroy();  
-    wall = createSprite(1500, 196, thickness, 322);
-    wall.shapeColor = "white";
-    if(bulletCreated === false) createBullet();
-    bullet.velocityX = speed;  
+    bullet.velocityX = speed;
     if(hasCollided(bullet, wall))
     {
       bullet.velocityX = 0;
+      bulletCreated = true;
       damage = (0.5 * weight * speed * speed)/ (thickness * thickness * thickness);
       if(damage > 10) wall.shapeColor = "red";
       else wall.shapeColor = "green";
@@ -58,7 +62,7 @@ function draw()
   }
     if(gameState === "end")
     {
-      bulletCreated = true;
+      thicknessChanged = false;
       if(damage > 10) fill("red");
       else fill("green");
       textFont("Lucida Calligraphy");
@@ -85,5 +89,4 @@ function createBullet()
   if(bullet != null) bullet.destroy();
   bullet = createSprite(50, 200, 10, 5);
   bullet.shapeColor = "white";
-  bulletCreated = true;
 }
